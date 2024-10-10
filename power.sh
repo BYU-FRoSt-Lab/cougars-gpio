41bc80bec3340194da79855452dea1e55a72f1aa
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-10 16:25:19 -0600

    Initial push

---
+++ power.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Created by Nelson Durrant, Sep 2024
+#
+# Toggles the power state of the Teensy 4.1
+# - Specify a power state using 'bash power.sh <state>'
+
+function printInfo {
+  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+}
+
+function printWarning {
+  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+}
+
+function printError {
+  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+}
+
+case $1 in
+    "on")
+
+        if [ -z "$(tycmd list | grep Teensy)" ]; then
+
+            sudo python3 ~/gpio/gpio_tools/toggle_power.py
+        else
+            echo ""
+            printInfo "Teensy is already powered on"
+            echo ""
+        fi
+        ;;
+    "off")
+
+        if [ -z "$(tycmd list | grep Teensy)" ]; then
+            echo ""
+            printInfo "Teensy is already powered off"
+            echo ""
+        else
+
+            sudo python3 ~/gpio/gpio_tools/toggle_power.py
+        fi
+        ;;
+    *)
+        echo ""
+        printWarning "No power state specified"
+        printWarning "Specify a power state using 'bash power.sh <state>'"
+        echo ""
+        ;;
+esac
---
+++ strobe.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Created by Nelson Durrant, Sep 2024
+#
+# Turns the strobe light on or off
+# - Specify a power state using 'bash strobe.sh <state>'
+
+function printInfo {
+  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+}
+
+function printWarning {
+  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+}
+
+function printError {
+  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+}
+
+case $1 in
+    "on")
+        sudo python3 ~/gpio/gpio_tools/strobe_on.py &
+        ;;
+    "off")
+        sudo killall python3
+        sudo python3 ~/gpio/gpio_tools/strobe_off.py
+        ;;
+    *)
+        echo ""
+        printWarning "No power state specified"
+        printWarning "Specify a power state using 'bash strobe.sh <state>'"
+        echo ""
+        ;;
+esac
