66a6d44fbbec5a17e9c26b97eb92a1a4824f3edb
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-20 17:43:45 -0700

    Permission fix updates

--- permission_fix.sh
+++ permission_fix.sh
@@ -2,7 +2,14 @@
 # Created by Nelson Durrant, Oct 2024
 #
 # Quick GPIO permission fix
+# - Specify the chip name using 'bash permission_fix.sh <chip>'
 
-source ~/config/bash_vars.sh
-
-sudo chmod 777 $GPIO_CHIP
+case $1 in
+  "")
+    printError "No GPIO chip specified"
+    printError "Specify the chip name using 'bash permission_fix.sh <chip>'"
+    ;;
+  *)
+    sudo chmod 777 $1
+    ;;
+esac
